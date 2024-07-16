72e25bebbfe1a46f1cd40c3a4d8e3b083e587a60
Simon Gloor <sgloor@outlook.com>
2024-07-16 23:45:00 +0200

    start working on mvp

--- frontend/genshin-upgrader-frontend/src/App.jsx
+++ frontend/genshin-upgrader-frontend/src/App.jsx
@@ -1,28 +1,17 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import artifacts from './data/mock/artifacts.json';
 import './styles/App.scss';
-import logo from './logo.svg';
 
 function App() {
+  console.log(artifacts);
+
+  // render
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="https://artiscan.ninjabay.org/#/artifacts/help" target="_blank" rel="noopener noreferrer">
+        Screengrab tool
+      </a>
     </div>
   );
 }
---
+++ frontend/genshin-upgrader-frontend/src/components/template.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+// import '../styles/Template.scss';
+
+export default function Template({ children }) {
+  return (
+    <div
+      className="Template"
+    >
+      {children}
+    </div>
+  );
+}
