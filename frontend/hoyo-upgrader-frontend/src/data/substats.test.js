5a10c49b0e270f64125a4bf70a9d3f81fc9f1e25
Simon Gloor <sgloor@outlook.com>
2024-08-13 10:17:41 +0200

    add tests for special case of 2 missing stats

--- frontend/hoyo-upgrader-frontend/src/data/substats.test.js
+++ frontend/hoyo-upgrader-frontend/src/data/substats.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable camelcase */
 /* eslint-disable no-undef */
 import { getRelevantSubstatsOfArtifact } from './substats';
@@ -25,8 +26,53 @@ const testSands_1 = {
     hp_: 0,
   },
 };
+const testSands_2 = {
+  piece: 'sands',
+  level: 0,
+  rarity: 4,
+  mainStatKey: 'enerRech_',
+  substats: [
+    { key: 'atk_', value: 4 },
+    { key: 'eleMas', value: 21 },
+  ],
+  substatCounts: {
+    atk: 0,
+    atk_: 1,
+    critDMG_: 0,
+    critRate_: 0,
+    def: 0,
+    def_: 0,
+    eleMas: 1,
+    enerRech_: 0,
+    hp: 0,
+    hp_: 0,
+  },
+};
+const testPlume_1 = {
+  piece: 'plume',
+  level: 0,
+  rarity: 4,
+  mainStatKey: 'atk',
+  substats: [
+    { key: 'enerRech_', value: 5.2 },
+    { key: 'hp_', value: 4.7 },
+  ],
+  substatCounts: {
+    atk: 0,
+    atk_: 0,
+    critDMG_: 0,
+    critRate_: 0,
+    def: 0,
+    def_: 0,
+    eleMas: 0,
+    enerRech_: 1,
+    hp: 0,
+    hp_: 1,
+  },
+};
 const testBuild_1 = { substats: ['enerRech_', 'critRate_', 'critDMG_'] };
 const testBuild_2 = { substats: ['enerRech_', 'critRate_', 'critDMG_', 'atk_', 'eleMas'] };
+const testBuild_3 = { substats: ['enerRech_', 'critRate_', 'hp_'] };
 
 test('gets correct impossibleSubstats case 1', () => {
   // should be 2 because only critRate_ and critDMG_ are possible on sands
@@ -70,4 +116,40 @@ test('gets correct assumedUsefulMissingSlots case 2', () => {
   const { assumedUsefulMissingSlots } = getRelevantSubstatsOfArtifact(testSands_1, testBuild_2);
   // should be 1 because the last slot could roll into critRate_
   expect(assumedUsefulMissingSlots).toEqual(1);
+});
+
+test('gets correct missingRollChances case 1', () => {
+  const { relevantSubstats } = getRelevantSubstatsOfArtifact(testSands_1, testBuild_1);
+  // should be 1 because the last slot could roll into critRate_
+  expect(relevantSubstats.missingRollChances[0]).toEqual(1.0);
+});
+test('gets correct missingRollChances case 2', () => {
+  const { relevantSubstats } = getRelevantSubstatsOfArtifact(testSands_1, testBuild_1);
+  // should be 0.5 because the 2nd roll could either be critRate_ or critDMG_
+  expect(relevantSubstats.missingRollChances[1]).toEqual(0.5);
+});
+test('gets correct missingRollChances case 3', () => {
+  const { relevantSubstats } = getRelevantSubstatsOfArtifact(testSands_2, testBuild_1);
+  // should be 1 because the 1st + 2nd rolls could go into critRate_ and critDMG_
+  expect(relevantSubstats.missingRollChances[1]).toEqual(1.0);
+});
+test('gets correct missingRollChances case 4', () => {
+  const { relevantSubstats } = getRelevantSubstatsOfArtifact(testSands_2, testBuild_1);
+  // should be 0.5 because the 3rd roll could go into critRate_ or critDMG_
+  expect(relevantSubstats.missingRollChances[2]).toEqual(0.5);
+});
+test('gets correct missingRollChances case 5', () => {
+  const { relevantSubstats } = getRelevantSubstatsOfArtifact(testPlume_1, testBuild_3);
+  // should be 1 because the 1th roll could go into critRate_
+  expect(relevantSubstats.missingRollChances[0]).toEqual(1.0);
+});
+test('gets correct missingRollChances case 6', () => {
+  const { relevantSubstats } = getRelevantSubstatsOfArtifact(testPlume_1, testBuild_3);
+  // should be 0 because the 2nd roll can't roll into anything useful
+  expect(relevantSubstats.missingRollChances[1]).toEqual(0.0);
+});
+test('gets correct missingRollChances case 7', () => {
+  const { relevantSubstats } = getRelevantSubstatsOfArtifact(testPlume_1, testBuild_3);
+  // should be 0.75 because the 3rd roll can roll into three valuable substats
+  expect(relevantSubstats.missingRollChances[2]).toEqual(0.75);
 });
