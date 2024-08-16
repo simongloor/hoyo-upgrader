a24e76b69c7fbd228c5a0bd933c05d686d3ea3d4
Simon Gloor <sgloor@outlook.com>
2024-08-17 00:34:50 +0200

    start adding premade builds

---
+++ frontend/hoyo-upgrader-frontend/src/data/kqmBuilds.js
@@ -0,0 +1,936 @@
+const kqmBuilds = {
+  Albedo: {
+    'Off-Field DPS': {
+      sets: ['GoldenTroupe'],
+      mainstats: {
+        sands: ['def_'],
+        goblet: ['geo_dmg_'],
+        circlet: ['critRate_', 'critDMG_'],
+      },
+      substats: ['enerRech_', 'critRate_', 'critDMG_', 'def_'],
+    },
+  },
+  Alhaitham: {
+    'On-Field DPS': {
+      sets: ['GildedDreams'],
+      mainstats: {
+        sands: ['eleMas'],
+        goblet: ['dendro_dmg_'],
+        circlet: ['critRate_', 'critDMG_'],
+      },
+      substats: ['enerRech_', 'critRate_', 'critDMG_', 'eleMas', 'atk_'],
+    },
+  },
+  Aloy: {},
+  Amber: {},
+  AratakiItto: {
+    'On-Field DPS': {
+      sets: ['HuskOfOpulentDreams'],
+      mainstats: {
+        sands: ['def_'],
+        goblet: ['geo_dmg_'],
+        circlet: ['critRate_', 'critDMG_'],
+      },
+      substats: ['enerRech_', 'critRate_', 'critDMG_', 'def_'],
+    },
+  },
+  Arlecchino: {
+    'Mono Pyro': {
+      sets: ['FragmentOfHarmonicWhimsy', 'GladiatorsFinale'],
+      mainstats: {
+        sands: ['atk_'],
+        goblet: ['pyro_dmg_'],
+        circlet: ['critRate_', 'critDMG_'],
+      },
+      substats: ['enerRech_', 'critRate_', 'critDMG_', 'atk_'],
+    },
+    Vaporize: {
+      sets: ['FragmentOfHarmonicWhimsy', 'GladiatorsFinale'],
+      mainstats: {
+        sands: ['atk_', 'eleMas'],
+        goblet: ['pyro_dmg_'],
+        circlet: ['critRate_', 'critDMG_'],
+      },
+      substats: ['enerRech_', 'critRate_', 'critDMG_', 'atk_', 'eleMas'],
+    },
+  },
+  Baizhu: {
+    'Healing (without Fav)': {
+      sets: ['DeepwoodMemories', 'OceanHuedClam'],
+      mainstats: {
+        sands: ['enerRech_'],
+        goblet: ['hp_'],
+        circlet: ['hp_'],
+      },
+      substats: ['enerRech_', 'hp_'],
+    },
+  },
+  Barbara: {
+    'Healing (without Fav)': {
+      sets: ['OceanHuedClam'],
+      mainstats: {
+        sands: ['hp_'],
+        goblet: ['hp_'],
+        circlet: ['heal_'],
+      },
+      substats: ['hp_'],
+    },
+  },
+  Beidou: {
+    'Off-Field DPS': {
+      sets: ['EmblemOfSeveredFate', 'GladiatorsFinale', 'ThunderingFury', 'NoblesseOblige'],
+      mainstats: {
+        sands: ['enerRech_'],
+        goblet: ['electro_dmg_'],
+        circlet: ['critRate_', 'critDMG_'],
+      },
+      substats: ['enerRech_', 'critRate_', 'critDMG_', 'atk_'],
+    },
+  },
+  Bennett: {
+    Healing: {
+      sets: ['NoblesseOblige'],
+      mainstats: {
+        sands: ['enerRech_'],
+        goblet: ['hp_'],
+        circlet: ['hp_', 'heal_'],
+      },
+      substats: ['enerRech_', 'hp_'],
+    },
+    'Off-Field DPS': {
+      sets: ['NoblesseOblige'],
+      mainstats: {
+        sands: ['enerRech_', 'atk_', 'eleMas'],
+        goblet: ['pyro_dmg_'],
+        circlet: ['critRate_', 'critDMG_'],
+      },
+      substats: ['enerRech_', 'critRate_', 'critDMG_', 'atk_', 'eleMas'],
+    },
+  },
+  Candace: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Charlotte: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Chevreuse: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Chiori: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Chongyun: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Clorinde: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Collei: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Cyno: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Dehya: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Diluc: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Diona: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Dori: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Emilie: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Eula: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Faruzan: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Fischl: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Freminet: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Furina: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Gaming: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Ganyu: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Gorou: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  HuTao: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Jean: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  KaedeharaKazuha: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Kaeya: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  KamisatoAyaka: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  KamisatoAyato: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Kaveh: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Keqing: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Kirara: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Klee: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  KujouSara: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  KukiShinobu: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Layla: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Lisa: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Lynette: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Lyney: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Mika: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Mona: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Nahida: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Navia: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Neuvillette: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Nilou: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Ningguang: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Noelle: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Qiqi: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  RaidenShogun: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Razor: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Rosaria: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  SangonomiyaKokomi: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Sayu: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Sethos: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Shenhe: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  ShikanoinHeizou: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Sigewinne: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Sucrose: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Tartaglia: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Thoma: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Tighnari: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Traveler: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Venti: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Wanderer: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Wriothesley: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Xiangling: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Xianyun: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Xiao: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Xingqiu: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Xinyan: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  YaeMiko: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Yanfei: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Yaoyao: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Yelan: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Yoimiya: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  YunJin: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+  Zhongli: {
+    // '': {
+    //   sets: [''],
+    //   mainstats: {
+    //     sands: [''],
+    //     goblet: [''],
+    //     circlet: [''],
+    //   },
+    //   substats: [''],
+    // },
+  },
+};
+export default kqmBuilds;
